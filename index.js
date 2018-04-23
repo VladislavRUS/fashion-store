9c1ce8f8b053500052549a9ba307e638e10e1ecf
Владислав Курочкин <vlad555.95@mail.ru>
2018-04-24 01:06:34 +0400

    for heroku

--- index.js
+++
@@ -1,7 +0,0 @@
-const static = require('node-static');
-const file = new static.Server();
-
-require('http').createServer((request, response) => 
-  request
-  .addListener('end', () => file.serve(request, response))
-  .resume()).listen(process.env.PORT || 3000);
